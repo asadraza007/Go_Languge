118c4328b15d28f8f47e9fe8076c3654398f0091
Asad Raza <Asad Raza>
2017-04-25 17:26:02 +0500

    Encryption and decryption

---
+++ encryption_decryption.go
@@ -0,0 +1,62 @@
+////////////////////////////////
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+)
+
+//Encrypt key,Text []byte
+func Encrypt(key, text []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	b := base64.StdEncoding.EncodeToString(text)
+	ciphertext := make([]byte, aes.BlockSize+len(b))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
+	return ciphertext, nil
+}
+
+//Decrypt key,Text []byte
+func Decrypt(key, text []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	if len(text) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+	iv := text[:aes.BlockSize]
+	text = text[aes.BlockSize:]
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(text, text)
+	data, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func main() {
+	//any 32 byte key
+	key := []byte("sdfghjklzxcvbnmqwertyuiop1234567")
+	text := []byte("Hello World")
+	encrypted_text, _ := Encrypt(key, text)
+	decrypted_text, _ := Decrypt(key, encrypted_text)
+
+	fmt.Println("Text:", string(text))
+	fmt.Println("Encrypted Text:", string(encrypted_text))
+	fmt.Println("Decrypted Text:", string(decrypted_text))
+
+}
